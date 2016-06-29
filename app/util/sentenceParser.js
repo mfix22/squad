55e43e649f1a04b9ff0be14911f594f33e9e0e10
Michael Fix <mfix@wisc.edu>
2016-06-29 12:19:00 -0700

    create sentence highlighter that finds start and end times and dates. Need to create moment event object from times

--- app/util/sentenceParser.js
+++ app/util/sentenceParser.js
@@ -3,21 +3,42 @@ var S = require('string');
 
 var HUMAN_READABLE = 'dddd, MMMM Do YYYY, h:mma'
 var time_reg = /\b((0*\d)|(1[0-2]))(:\d\d(pm|am|p|a)*|pm|am|p|a)\b|noon|midnight/ig;
-var address_reg = /at\s(.*?)(?:(?!\sat).)*/ig;
+var address_reg = /at\s(.*?)(?:(?!(\sat|\sfrom)).)*/ig;
 var date_reg = /(\d{1,2}[\/-]\d{1,2}[\/-](\d{4}|\d{2})\b)|(\d{1,2}[\/-]\d{1,2}\b)|today|tomorrow/ig;
 
-var sentence = "Meeting from 6-2 at 888 Brannan ";
+function highlight(sentence) {
+	  var parsed = {};
+	  var m = moment();
+		var address;
+	  var addressPoss = sentence.match(address_reg);
+	  if (addressPoss && addressPoss.length) {
+	    if (addressPoss.length == 1) {
+				if (!time_reg.test(addressPoss[0])) address = S(addressPoss[0]).chompLeft('at').s.trim();
+	    } else{
+				for (var i = 0; i < addressPoss.length; i++) {
+					if (!time_reg.test(addressPoss[i])) {
+						address = S(addressPoss[i]).chompLeft('at').s.trim();
+						break;
+					}
+				}
+	    }
+	  }
+	  var times = sentence.match(time_reg);
+	  var date = sentence.match(date_reg);
+	  if (date) {
+	    parsed.startDate_regex = date[0];
+			parsed.endDate_regex = date[1];
+	  }
+	  if (times) {
+	    parsed.startTime_regex = times[0];
+			parsed.endTime_regex = times[1];
+	  }
+		parsed.location = address;
 
-function buildTime(time) {
-  if (time.toLowerCase() == 'noon') {
-    return '12:00';
-  } else if (time.toLowerCase() == 'midnight'){
-    return '00:00';
-  } else {
-    return moment(time, ['h:mma','ha']).format('HH:mm');
-  }
-}
+	  return parsed;
+	}
 
+// FIXME - needs refactoring badly
 function parse(sentence) {
   var parsed = {};
   var m = moment();
@@ -43,14 +64,14 @@ function parse(sentence) {
   builder+='T';
   if (firstTime) {
     parsed.startTime_regex = firstTime;
-    startTime = builder + buildTime(firstTime) +":00"
+    startTime = builder + _buildTime(firstTime) +":00"
   } else {
     startTime =  builder +  moment().format('HH:mm:ss')
   }
   startTime = moment(startTime);
   if (secondTime) {
     parsed.endTime_regex = secondTime;
-    endTime = moment(builder + buildTime(secondTime) + ":00");
+    endTime = moment(builder + _buildTime(secondTime) + ":00");
   } else {
     endTime =  startTime.add(1,'h');
   }
@@ -62,7 +83,9 @@ function parse(sentence) {
 }
 
 module.exports.parse = parse;
+module.exports.highlight = highlight;
 
+// var sentence = "Meeting on 6-2 at 888 Brannan ";
 // var parsed = parse(sentence);
 // console.log(JSON.stringify(parsed, null, 4));
 // console.log(parsed.startTime.format(HUMAN_READABLE));
