40d75e0b43fa803f07036a2caefd9e2e2dc25975
Fix <nfix@SDGL1498ec027.local>
2016-08-06 16:04:05 -0700

    config webpack

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -7,7 +7,7 @@ module.exports = webpackMerge(commonConfig, {
   devtool: 'cheap-module-eval-source-map',
 
   output: {
-    path: helpers.root('dist'),
+    path: helpers.root('public'),
     publicPath: 'http://localhost:8080/',
     filename: '[name].js',
     chunkFilename: '[id].chunk.js'
