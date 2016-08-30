9540ea42baa612f80f027b38ac31ac0d1769d165
Michael Fix <mfix@wisc.edu>
2016-08-30 15:54:31 -0500

    recreate form without Bootstrap

--- client-app/src/components/EventForm.jsx
+++ client-app/src/components/EventForm.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
 import '../styles/form'
 
+// import '../vendor/cal-form.js'
+
 let EventForm = React.createClass({
+  componentDidMount : function() {
+    console.log('Mounted');
+    require('../vendor/cal-form.js')();
+  },
+
   render: function() {
     return (
       <div className="form-container focus">
@@ -16,35 +23,28 @@ let EventForm = React.createClass({
         <div className="cal-form-submit">
           <i className="ion-checkmark"></i>
         </div>
-        <div className="row">
-          <div className="cal-form-buttons what-button flex-center col-xs-12 text-center">
-            <div id="sentence" className="sentence what-button-text" contentEditable="true" placeholder="[ TYPE CALENDAR INFO HERE ]"></div>
+
+        <div className="cal-form-buttons what-button">
+          <div id="sentence" className="sentence what-button-text" contentEditable="true" placeholder="[ TYPE CALENDAR INFO HERE ]"></div>
+        </div>
+        <div className="cal-form-buttons cal-form-bottom-buttons when-button">
+          <div className="form-times startDate"></div>
+          <div className="form-times endDate"></div>
+          <div className="form-times startTime"></div>
+          <div className="form-times endTime"></div>
+        </div>
+        <div className="cal-form-buttons cal-form-bottom-buttons">
+          <div className="with-button">
+            <div className="with-button-node add">
+              <i className="ion-person-add"></i>
+            </div>
           </div>
         </div>
-        <div className="row">
-          <div className="cal-form-buttons cal-form-bottom-buttons col-xs-4 text-center">
-            <div className="row">
-              <div className="col-xs-6  form-times startDate"></div>
-              <div className="col-xs-6  form-times endDate"></div>
-            </div>
-            <div className="row">
-              <div className="col-xs-6  form-times startTime"></div>
-              <div className="col-xs-6  form-times endTime"></div>
-            </div>
-          </div>
-          <div className="cal-form-buttons cal-form-bottom-buttons col-xs-4 text-center">
-            <div className="with-button">
-              <div className="with-button-node add">
-                <i className="ion-person-add"></i>
-              </div>
-            </div>
-          </div>
-          <div className="cal-form-buttons cal-form-bottom-buttons col-xs-4 text-center">
-            <div className="where-button">
-              <h6 className="where-button-text"></h6>
-              <i className="ion-android-pin"></i>
-              <input placeholder="YOUR LOCATION HERE"type="text" size="30" className="location-input"/>
-            </div>
+        <div className="cal-form-buttons cal-form-bottom-buttons">
+          <div className="where-button">
+            <h6 className="where-button-text"></h6>
+            <i className="ion-android-pin"></i>
+            <input placeholder="YOUR LOCATION HERE"type="text" size="30" className="location-input"/>
           </div>
         </div>
       </div>
