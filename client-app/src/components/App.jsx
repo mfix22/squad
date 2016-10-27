df44870f5cea16b0a87661204b8f3000db857e5d
Michael Fix <mfix@wisc.edu>
2016-10-27 00:09:36 -0500

    Add today button

--- client-app/src/components/App.jsx
+++ client-app/src/components/App.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import Calendar from './Calendar'
 import NextButton from './Buttons/NextButton'
 import PrevButton from './Buttons/PrevButton'
+import TodayButton from './Buttons/TodayButton'
 
 const App = () => (
   <div>
     <PrevButton/>
+    <TodayButton/>
     <NextButton/>
     <Calendar />
   </div>
---
+++ client-app/src/components/Buttons/TodayButton.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+import PlainActionButton from './PlainActionButton'
+
+const TodayButton = () => (<PlainActionButton label="TODAY" action="VIEW_TODAY"/>)
+export default TodayButton
