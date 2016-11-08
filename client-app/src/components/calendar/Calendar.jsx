d5e212433de923985fe81e32fc37065075c358b1
Michael Fix <mfix@wisc.edu>
2016-11-08 16:09:12 -0600

    Move chunk day arrays out of calendar for use elsewhere

--- client-app/src/components/calendar/Calendar.jsx
+++ client-app/src/components/calendar/Calendar.jsx
@@ -1,45 +1,14 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import moment from 'moment'
 import DateColumn from './DateColumn'
 import WeekRow from './WeekRow'
 
+import { getChunkedDays } from '../../helpers/array'
+
 require('../../styles/app.scss');
 
-function getDateArray(numDays=7, startIndex=0){
-  return [...Array(numDays).keys()].map((key, index) => {
-    return moment().add(index + startIndex, 'd').format();
-  });
-}
-
-const chunk = (a, c) => {
-  return a.reduce((accum, next, i) => {
-    const intI = Math.floor(i / c)
-    return Object.assign([], accum, {
-      [intI] : [...accum[intI], next]
-    })
-  }, Array(Math.ceil(a.length / c)).fill([]))
-}
-
-function getDays(refDate, numDays = 42) {
-  // if numDays < 10, create a week view with dayOfTheWeek offset
-  if (numDays <= 10) return [[...Array(numDays).keys()].map((offset) => moment(refDate).day(offset).format())]
-  const numWeeks = Math.ceil(numDays / 7);
-  const correctedNumDays = numWeeks * 7
-
-  // chunks days into week arrays of day arrays
-  return [...Array(correctedNumDays).keys()].map(i => i - moment(refDate).date())
-                                   .map(offset => moment(refDate).day(offset).format())
-                                   .reduce((accum, next, i) => {
-                                     const intI = Math.floor(i / 7)
-                                     return Object.assign([], accum, {
-                                       [intI] : [...accum[intI], next]
-                                     })
-                                   }, Array(numWeeks).fill([]))
-}
-
 const Calendar = ({ events, referenceDate, daysInView }) => {
-  const days = getDays(referenceDate, daysInView);
+  const days = getChunkedDays(referenceDate, daysInView);
   return (
     <div className="module calendar">
       {
---
+++ client-app/src/helpers/array.js
@@ -0,0 +1,28 @@
+import moment from 'moment'
+
+export const chunk = (a, c) => {
+  return a.reduce((accum, next, i) => {
+    const intI = Math.floor(i / c)
+    return Object.assign([], accum, {
+      [intI] : [...accum[intI], next]
+    })
+  }, Array(Math.ceil(a.length / c)).fill([]))
+}
+
+export function getDays(refDate, numDays=42) {
+  if (numDays <= 10) return [...Array(numDays).keys()].map((offset) => moment(refDate).day(offset).format())
+  const numWeeks = Math.ceil(numDays / 7);
+  const correctedNumDays = numWeeks * 7
+
+  // chunks days into week arrays of day arrays
+  return [...Array(correctedNumDays).keys()].map(i => i - moment(refDate).date())
+                                   .map(offset => moment(refDate).day(offset).format())
+}
+
+export function getChunkedDays(refDate, numDays = 42) {
+  // if numDays < 10, create a week view with dayOfTheWeek offset
+  const days = getDays((refDate, numDays));
+  if (numDays <= 10) return [days]
+  // chunks days into week arrays of day arrays
+  return chunk(days, 7)
+}
