c0632f12436b99c5a38a1aefb80ac6bc7548e7d1
Michael Fix <mfix@wisc.edu>
2016-11-10 00:08:15 -0600

    Update control bar to fit design

--- client-app/src/helpers/array.js
+++ client-app/src/helpers/array.js
@@ -9,7 +9,9 @@ export const chunk = (a, c) => {
   }, Array(Math.ceil(a.length / c)).fill([]))
 }
 
-export function getDays(refDate, numDays=42) {
+const defaultNumDays = 35;
+
+export function getDays(refDate, numDays=defaultNumDays) {
   if (numDays <= 10) return [...Array(numDays).keys()].map((offset) => moment(refDate).day(offset).format())
   const numWeeks = Math.ceil(numDays / 7);
   const correctedNumDays = numWeeks * 7
@@ -19,7 +21,7 @@ export function getDays(refDate, numDays=42) {
                                    .map(offset => moment(refDate).day(offset).format())
 }
 
-export function getChunkedDays(refDate, numDays = 42) {
+export function getChunkedDays(refDate, numDays=defaultNumDays) {
   // if numDays < 10, create a week view with dayOfTheWeek offset
   const days = getDays((refDate, numDays));
   if (numDays <= 10) return [days]
