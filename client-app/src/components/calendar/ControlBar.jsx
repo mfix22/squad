bcdccfc9cf359d5e32d22b389278a5eac853d006
Jake Dexheimer <jakedex@gmail.com>
2016-11-13 13:27:01 -0600

    Control bar use Toolbar material ui

--- client-app/src/components/calendar/ControlBar.jsx
+++ client-app/src/components/calendar/ControlBar.jsx
@@ -3,36 +3,41 @@ import { connect } from 'react-redux'
 import moment from 'moment'
 
 import DropDownMenu from 'material-ui/DropDownMenu';
-import { ToolbarTitle } from 'material-ui/Toolbar';
+import { Toolbar, ToolbarTitle, ToolbarGroup } from 'material-ui/Toolbar';
 import MenuItem from 'material-ui/MenuItem';
 
 import NextButton from '../Buttons/NextButton'
 import PrevButton from '../Buttons/PrevButton'
 import TodayButton from '../Buttons/TodayButton'
+import { getOrderedMonthArray } from '../../helpers/util'
 
 const ControlBar = ({ referenceDate }) => (
-  <div className="controlBar">
-    <div className="refDateHeaderWrapper">
-      <ToolbarTitle style={{ fontSize : '16px' }} className="refDateHeader" text={moment(referenceDate).format("MMMM YYYY")}/>
-    </div>
-    <ToolbarTitle style={{ fontSize : '16px', fontWeight: "bold" }} className="rangeHeader" text="11/5 - 11/7" />
+  <Toolbar className="controlBar">
+    <ToolbarGroup firstChild className="refDateHeaderWrapper">
+      <DropDownMenu value={0} onChange={() => {}}>
+        {getOrderedMonthArray(moment(referenceDate).format('MMMM YYYY')).map((month, index) => {
+          return <MenuItem value={index} primaryText={month} />
+        })}
+      </DropDownMenu>
+    </ToolbarGroup>
+    <ToolbarTitle style={{ fontSize: '16px', fontWeight: 'bold' }} className="rangeHeader" text="11/5 - 11/7" />
     <div className="buttonContainer">
-      <DropDownMenu value={"4_DAY"} onChange={(event, index, value) => console.log(value)}>
-        <MenuItem value={"4_DAY"} primaryText="4 Day" />
-        <MenuItem value={"WEEK"} primaryText="Week" />
-        <MenuItem value={"2_WEEK"} primaryText="2 Week" />
-        <MenuItem value={"MONTH"} primaryText="Month" />
+      <DropDownMenu value="4_DAY" onChange={(event, index, value) => console.log(value)}>
+        <MenuItem value="4_DAY" primaryText="4 Day" />
+        <MenuItem value="WEEK" primaryText="Week" />
+        <MenuItem value="2_WEEK" primaryText="2 Week" />
+        <MenuItem value="MONTH" primaryText="Month" />
       </DropDownMenu>
       <PrevButton />
       <TodayButton />
       <NextButton />
     </div>
-  </div>
+  </Toolbar>
 )
 
 const mapStateToProps = (state) => {
   return {
-    referenceDate : state.date
+    referenceDate: state.date
   }
 }
 
