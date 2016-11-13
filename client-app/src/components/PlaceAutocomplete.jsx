d190dc356b0cd2244ca896f056dacb656e4898c1
Jake Dexheimer <jakedex@gmail.com>
2016-11-12 23:31:08 -0600

    Start Placeautocomplete component

---
+++ client-app/src/components/PlaceAutocomplete.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import GooglePlaceAutocomplete from 'googlePlaceAutocomplete'
+
+class PlaceAutocomplete extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      value: '',
+    }
+
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(event) {
+    this.setState({
+      value: event.target.value,
+    })
+  }
+
+  render() {
+    return (
+      <GooglePlaceAutocomplete
+        hintText={this.props.hintText}
+        onChange={this.handleChange}
+        onNewRequest={() => {}}
+        name={'location'}
+      />
+    )
+  }
+}
+
+export default PlaceAutocomplete
