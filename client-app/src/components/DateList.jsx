d9bae005f5b02ecb15f27098ee02bd058ccf7499
Jake Dexheimer <jakedex@gmail.com>
2016-11-13 11:50:50 -0600

    DateList -> chips, mostly done

--- client-app/src/components/DateList.jsx
+++ client-app/src/components/DateList.jsx
@@ -1,8 +1,54 @@
 import React from 'react'
-import { List, ListItem } from 'material-ui/List'
+import Chip from 'material-ui/Chip'
 import DatePicker from 'material-ui/DatePicker'
 import TimePicker from 'material-ui/TimePicker'
+import FlatButton from 'material-ui/FlatButton'
 import Moment from 'moment'
+
+import Label from './Label'
+import PlainActionButton from './Buttons/PlainActionButton'
+
+const style = {
+  form: {
+    position: 'relative',
+    paddingTop: '12px',
+    marginTop: '16px'
+  },
+  timePicker: {
+    width: '88px',
+    display: 'block',
+  },
+  timeFrom: {
+    position: 'absolute',
+  },
+  timeTo: {
+    marginLeft: '104px',
+  },
+  submit: {
+    cursor: 'pointer',
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    right: 0,
+    left: 0,
+    width: '100%',
+    opacity: 0,
+  },
+  chips: {
+    marginTop: '8px',
+    display: 'flex',
+    flexWrap: 'wrap'
+  },
+  chip: {
+    margin: '4px',
+  }
+}
+
+const optionToDisplayString = (option) => {
+  return `${Moment(option.date).format('MMM Do YY')},
+    ${Moment(option.timeFrom).format('h:mmA')} -
+   ${Moment(option.timeTo).format('h:mmA')}`
+}
 
 class DatePickerWithList extends React.Component {
   constructor(props) {
@@ -15,6 +61,7 @@ class DatePickerWithList extends React.Component {
     this.handleChangeTimeTo = this.handleChangeTimeTo.bind(this)
     this.handleChangeDate = this.handleChangeDate.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleChipDelete = this.handleChipDelete.bind(this)
   }
 
   handleChangeTimeTo(event, time) {
@@ -51,23 +98,35 @@ class DatePickerWithList extends React.Component {
     event.preventDefault()
   }
 
+  handleChipDelete(index) {
+    console.log(index)
+    const arr = this.state.options
+    arr.splice(index, 1)
+    this.setState({
+      options: arr
+    })
+  }
+
   render() {
     return (
       <div>
-        <form onSubmit={this.handleSubmit}>
+        <form style={style.form} onSubmit={this.handleSubmit}>
+          <Label labelFor="TimePicker" text="When" />
           <TimePicker
             hintText={this.props.hintTextTimeFrom}
-            // floatingLabelText={this.props.label}
-            // floatingLabelFixed
+            container="inline"
             value={this.state.timeFrom}
             onChange={this.handleChangeTimeFrom}
+            textFieldStyle={style.timePicker}
+            style={style.timeFrom}
           />
           <TimePicker
             hintText={this.props.hintTextTimeTo}
-            // floatingLabelText={this.props.label}
-            // floatingLabelFixed
+            container="inline"
             value={this.state.timeTo}
             onChange={this.handleChangeTimeTo}
+            textFieldStyle={style.timePicker}
+            style={style.timeTo}
           />
           <DatePicker
             value={this.state.date}
@@ -77,14 +136,18 @@ class DatePickerWithList extends React.Component {
             mode="landscape"
             onChange={this.handleChangeDate}
           />
-          <input type="submit" value="Submit" />
+          {/* <PlainActionButton label="Add option" onClick={this.handleSubmit} action="ADD_OPTION">
+            <input type="submit" value="Submit" style={style.submit} />
+          </PlainActionButton> */}
+          <FlatButton label="Add option" onClick={this.handleSubmit}></FlatButton>
+
         </form>
-        <List>
+        <div style={style.chips}>
           {this.state.options.map((option, index) => {
             console.log(this.state.options)
-            return <ListItem key={index} primaryText={option.toString()} />
+            return <Chip key={index} style={style.chip} onRequestDelete={() => this.handleChipDelete(index)}>{optionToDisplayString(option)}</Chip>
           })}
-        </List>
+        </div>
       </div>
     )
   }
