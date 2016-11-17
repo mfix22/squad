2da5fa6e156feef8359cc8eb9b97c9060215f8cf
Mike Fix <mrfix84@gmail.com>
2016-11-16 20:08:41 -0600

    Clean up reducers

--- client-app/src/reducers/events.js
+++ client-app/src/reducers/events.js
@@ -1,26 +1,7 @@
 import { RECEIVE_EVENT } from '../actions'
 
-// const singleEvent = (state, action) => {
-//   switch (action.type) {
-//     case ADD_EVENT :
-//       return {
-//         id : v4(),
-//         title : action.title,
-//         time : action.time,
-//         location : action.location || ""
-//       }
-//     default:
-//       return state;
-//   }
-// }
-
 const events = (state = [], action) => {
   switch (action.type) {
-    // case ADD_EVENT :
-    //   return [
-    //     ...state,
-    //     // singleEvent(null, action)
-    //   ]
     case RECEIVE_EVENT: {
       if (!action.events) return state
       return action.events
