45d71d39469a1ac2625ab09e0d445c7ae8663307
Jake Dexheimer <jakedex@gmail.com>
2016-12-02 20:13:26 -0600

    Start revamped AppBar

--- client/components/AppBar.jsx
+++ client/components/AppBar.jsx
@@ -1,20 +1,46 @@
 import React from 'react'
-import MaterialAppBar from 'material-ui/AppBar'
+import IconButton from 'material-ui/IconButton'
+import NavigationMenu from 'material-ui/svg-icons/navigation/menu'
 
+import Logo from './Logo'
+import SchedulingStepper from './SchedulingStepper'
+import NotificationsButton from './buttons/NotificationsButton'
+import AvatarButton from './buttons/AvatarButton'
 import { color } from '../vars'
-
-const AppBar = () => (
-  <MaterialAppBar
-    title="Squad"
-    style={style.appBar}
-    iconClassNameRight="muidocs-icon-navigation-expand-more"
-  />
-)
 
 const style = {
   appBar: {
-    backgroundColor: color.green,
+    transition: 'all 450ms cubic-bezier(0.23, 1, 0.32, 1) 0ms',
+    position: 'relative',
+    zIndex: 1100,
+    width: '100%',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingLeft: '24px',
+    paddingRight: '24px',
+    height: '64px'
   },
+  container: {
+    display: 'flex',
+    alignItems: 'center'
+  }
 }
+
+const AppBar = () => (
+  <div style={style.appBar}>
+    <div style={style.container}>
+      <IconButton>
+        <NavigationMenu />
+      </IconButton>
+      <Logo />
+    </div>
+    <SchedulingStepper activeStep={1} />
+    <div style={style.container}>
+      <NotificationsButton />
+      <AvatarButton src="./images/user-jake.png" />
+    </div>
+  </div>
+)
 
 export default AppBar
