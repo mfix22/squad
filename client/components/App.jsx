0f703ecdeb7a7047ee5d578380cf1cf245cb5442
Mike Fix <mrfix84@gmail.com>
2016-12-04 18:14:55 -0600

    Move app provider, router, and theme to App, out of index

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,10 +1,45 @@
 import React from 'react'
+import { Provider } from 'react-redux'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import getMuiTheme from 'material-ui/styles/getMuiTheme'
+import injectTapEventPlugin from 'react-tap-event-plugin'
+import { Router, Route, browserHistory } from 'react-router'
 
 import Scheduler from './pages/Scheduler'
+import { color } from '../vars'
 
-const App = (params) => {
-  console.log(params)
-  return <Scheduler />
-}
+import configureStore from '../helpers/configureStore'
+import { loadGoogleEvents } from '../api'
+import { loadState, saveState } from '../helpers/localStorage'
+
+const store = configureStore(loadState())
+
+store.subscribe(() => {
+  saveState(store.getState())
+})
+
+store.getState().users.forEach((user) => {
+  store.dispatch(loadGoogleEvents(user))
+})
+
+injectTapEventPlugin()
+
+const muiTheme = getMuiTheme({
+  palette: {
+    primary1Color: color.green,
+    accent1Color: color.green,
+  }
+})
+
+const App = () => (
+  <Provider store={store}>
+    <MuiThemeProvider muiTheme={muiTheme}>
+      <Router history={browserHistory}>
+        <Route path="/new" component={Scheduler} />
+        <Route path="/:event_id(#share)" component={Scheduler} />
+      </Router>
+    </MuiThemeProvider>
+  </Provider>
+)
 
 export default App
--- client/index.jsx
+++ client/index.jsx
@@ -1,44 +1,9 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { Provider } from 'react-redux'
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
-import getMuiTheme from 'material-ui/styles/getMuiTheme'
-import injectTapEventPlugin from 'react-tap-event-plugin'
-import { Router, Route, browserHistory } from 'react-router'
 
-import Scheduler from './components/pages/Scheduler'
-import configureStore from './helpers/configureStore'
-import { loadGoogleEvents } from './api'
-import { color } from './vars'
-import { loadState, saveState } from './helpers/localStorage'
-
-injectTapEventPlugin()
-
-const store = configureStore(loadState())
-
-store.subscribe(() => {
-  saveState(store.getState())
-})
-
-store.getState().users.forEach((user) => {
-  store.dispatch(loadGoogleEvents(user))
-})
-
-const muiTheme = getMuiTheme({
-  palette: {
-    primary1Color: color.green,
-    accent1Color: color.green,
-  }
-})
+import App from './components/App'
 
 render(
-  <Provider store={store}>
-    <MuiThemeProvider muiTheme={muiTheme}>
-      <Router history={browserHistory}>
-        <Route path="/new" component={Scheduler} />
-        <Route path="/:event_id(#share)" component={Scheduler} />
-      </Router>
-    </MuiThemeProvider>
-  </Provider>,
+  <App />,
   document.getElementById('root')
 )
