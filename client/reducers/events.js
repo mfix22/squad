be7c45287c87f9ffa6c90055b15a7d0eff0f33e1
Mike Fix <mrfix84@gmail.com>
2016-12-12 23:59:52 -0600

    Update reducers to safely receive events

--- client/reducers/events.js
+++ client/reducers/events.js
@@ -1,10 +1,11 @@
+import unionWith from 'lodash/unionWith'
 import { RECEIVE_EVENTS } from '../actions'
 
 const events = (state = [], action) => {
   switch (action.type) {
     case RECEIVE_EVENTS: {
       if (!action.events) return state
-      return action.events
+      return unionWith(state, action.events, (first, second) => first.id === second.id)
     }
     default:
       return state
--- client/reducers/users.js
+++ client/reducers/users.js
@@ -3,7 +3,7 @@ import { ADD_USER, RECEIVE_EVENT } from '../actions'
 const users = (state = [], action) => {
   switch (action.type) {
     case RECEIVE_EVENT:
-      if (action.users) return Array.from(new Set([...state, ...action.users]))
+      if (action.users) return action.users
       return state
     case ADD_USER: {
       if (!action.user) return state
