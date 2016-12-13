e556734fb731556f012cd338a42d2f8553b102e6
Mike Fix <mrfix84@gmail.com>
2016-12-12 20:32:16 -0600

    revert <Scheduler/> to be pure function
    Add Slack notifications for Travis

--- client/components/pages/Scheduler.jsx
+++ client/components/pages/Scheduler.jsx
@@ -1,19 +1,8 @@
 import React from 'react'
-import { withRouter } from 'react-router'
 import Base from './Base'
 
-class Scheduler extends React.Component {
+const Scheduler = ({ params, children }) => (
+  <Base params={params}>{children}</Base>
+)
 
-  componentDidMount() {
-    this.props.router.setRouteLeaveHook(this.props.route, () => {
-      return 'You have unsaved information, are you sure you want to leave this page?'
-    })
-  }
-
-  render() {
-    const { params, children } = this.props
-    return <Base params={params}>{children}</Base>
-  }
-}
-
-export default withRouter(Scheduler)
+export default Scheduler
